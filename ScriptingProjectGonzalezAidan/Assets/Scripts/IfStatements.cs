72c2546caa2883b68ab8fadc3a93cd17d08e8831
AidanGonzalez <724624@student.dusd.net>
2022-10-31 10:00:30 -0700

    started new script
    finished if statements and began loops

--- ScriptingProjectGonzalezAidan/Assets/Scripts/IfStatements.cs
+++ ScriptingProjectGonzalezAidan/Assets/Scripts/IfStatements.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 public class IfStatements : MonoBehaviour
 {
-    float coffeTemprature = 85.0f;
-    float hotLimitTemprature = 70.0f;
-    float coldLimitTemprature = 40.0f;
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            TempratureTest();
-        coffeTemprature -= Time.deltaTime * 5f;
-        
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                TemperatureTest();
+
+            coffeeTemperature -= Time.deltaTime * 5f;
+        }
+
+
+        void TemperatureTest()
+        {
+            // If the coffee's temperature is greater than the hottest drinking temperature...
+            if (coffeeTemperature > hotLimitTemperature)
+            {
+                // ... do this.
+                print("Coffee is too hot.");
+            }
+            // If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+            else if (coffeeTemperature < coldLimitTemperature)
+            {
+                // ... do this.
+                print("Coffee is too cold.");
+            }
+            // If it is neither of those then...
+            else
+            {
+                // ... do this.
+                print("Coffee is just right.");
+            }
+        }
     }
-    void TempratureTest()
-    {
-        if (coffeTemprature)
-    }
+
 }
