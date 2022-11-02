bc98d4c457c9c1ba79f99c44e337e52cf029a186
AidanGonzalez <724624@student.dusd.net>
2022-11-02 08:41:31 -0700

    started loops

--- ScriptingProjectGonzalezAidan/Assets/Scripts/ForLoop.cs
+++ ScriptingProjectGonzalezAidan/Assets/Scripts/ForLoop.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Loops : MonoBehaviour
+public class ForLoop : MonoBehaviour
 {
     int numEnemies = 3
     // Start is called before the first frame update
     void Start()
     {
-        for(int i= 0; i < numEnemies; i++)
+        for (int i = 0; i < numEnemies; i++)
         {
-            Debug.Log("creating enemy number: ")
+            Debug.Log("creating enemy number: " + i);
         }
     }
 
