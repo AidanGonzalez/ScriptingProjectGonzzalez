5dbc669a11e6711b4a46d4f63e2dc40d6eae746f
AidanGonzalez <724624@student.dusd.net>
2022-11-02 09:12:37 -0700

    finished loops
    Finished while loop, do while loop, and for each loop.

--- ScriptingProjectGonzalezAidan/Assets/Scripts/ForLoop.cs
+++ ScriptingProjectGonzalezAidan/Assets/Scripts/ForLoop.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ForLoop : MonoBehaviour
 {
-    int numEnemies = 3
+    int numEnemies = 3;
     // Start is called before the first frame update
     void Start()
     {
