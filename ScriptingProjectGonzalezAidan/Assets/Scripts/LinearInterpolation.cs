a37216e7bd1ada999d6bb1c28fd3291c9417bbb1
AidanGonzalez <724624@student.dusd.net>
2023-02-02 09:10:12 -0800

    .

---
+++ ScriptingProjectGonzalezAidan/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    // Start is called before the first frame update
+    void Start()
+    {
+        // In this case, result = 4
+        math.lerp takes 3 float parameters one representing the value to interpolate from another representing the value to interpolate to and a final float representing how far to interpolate.
+        float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+       Vector3.lerp works the same as math.lerp except instead of using float parameters it uses vector3.
+        Vector3 from = new Vector3(1f, 2f, 3f);
+        Vector3 to = new Vector3(5f, 6f, 7f);
+
+        // Here result = (4, 5, 6)
+        Vector3 result = Vector3.Lerp(from, to, 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        This happens per frame
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        
+        This happens per second
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+
+    }
+    */
+}
